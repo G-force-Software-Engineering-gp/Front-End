4e31c80d83b46ee2a4c573b38e7aeaa9680dca2b
Aryan Sabet Nejad <74782534+aryansabet@users.noreply.github.com>
2023-10-13 11:30:10 +0330

    feat: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { ModeToggle } from '@/components/ui/mode-toggle';
+import { QueryClient, QueryClientProvider, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import MyTest from './mytest';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ModeToggle />
-      <MyTest />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <ModeToggle />
+        <MyTest />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
