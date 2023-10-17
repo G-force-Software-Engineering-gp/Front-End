aba7779fa3f0e20789bb05dfab162e6d9edbb579
matin2001 <matin1380day@gmail.com>
2023-10-17 12:40:14 +0330

    chore: Homepage responsive solved

--- src/pages/HomePage/HomePageDetails.tsx
+++ src/pages/HomePage/HomePageDetails.tsx
@@ -145,8 +145,8 @@ const HomePageDetails = () => {
         </div>
         {/* Workspaces */}
         <div className="mt-4">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
+          <div className="sm:flex grid sm:items-center sm:justify-between">
+            <div className="flex items-center mb-4 sm:mb-0">
               <Avatar className="mr-2 h-8 w-8 rounded-sm ">
                 <AvatarImage className="rounded-sm " src="https://github.com/shadcn.png11" alt="@shadcn" />
                 <AvatarFallback className="rounded-sm ">G</AvatarFallback>
@@ -154,7 +154,7 @@ const HomePageDetails = () => {
               <div>WorkSpace 2</div>
             </div>
             <div className="">
-              <Button variant="secondary" className="mr-1 p-2">
+              <Button variant="secondary" className="mr-1 p-2 mt-1 sm:mt-0">
                 {' '}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -175,7 +175,7 @@ const HomePageDetails = () => {
                 Boards
               </Button>
 
-              <Button variant="secondary" className="mr-1 p-2">
+              <Button variant="secondary" className="mr-1 p-2 mt-1 sm:mt-0">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -193,7 +193,7 @@ const HomePageDetails = () => {
                 Highlights
               </Button>
 
-              <Button variant="secondary" className="mr-1 p-2">
+              <Button variant="secondary" className="mr-1 p-2 mt-1 sm:mt-0">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -214,7 +214,7 @@ const HomePageDetails = () => {
                 Views
               </Button>
 
-              <Button variant="secondary" className="mr-1 p-2">
+              <Button variant="secondary" className="mr-1 p-2 mt-1 sm:mt-0">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -234,7 +234,7 @@ const HomePageDetails = () => {
                 Members
               </Button>
 
-              <Button variant="secondary" className="p-2">
+              <Button variant="secondary" className="p-2 mt-1 sm:mt-0">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -254,6 +254,7 @@ const HomePageDetails = () => {
               </Button>
             </div>
           </div>
+
           <div className="mb-8 mt-4 grid auto-rows-fr sm:grid-cols-2 gap-2">
             <Card className=" cursor-pointer bg-slate-200 dark:bg-slate-900">
               <CardHeader className="flex justify-between gap-4 space-y-0 p-4">
@@ -300,155 +301,14 @@ const HomePageDetails = () => {
               </CardHeader>
             </Card> */}
             <Card className=" cursor-pointer bg-slate-300 dark:bg-slate-800">
-              <div className=" gap-4 space-y-0 flex py-9 px-20">
+              <div className=" gap-4 space-y-0 flex sm:py-9 py-10 sm:px-20 px-12">
                 <label className=' text-xl font-semibold'>Create a new Board</label>
               </div>
             </Card>
           </div>
         </div>
 
-        <div className="mt-4">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <Avatar className="mr-2 h-8 w-8 rounded-sm ">
-                <AvatarImage className="rounded-sm " src="https://github.com/shadcn.png11" alt="@shadcn" />
-                <AvatarFallback className="rounded-sm ">G</AvatarFallback>
-              </Avatar>
-              <div>WorkSpace 1</div>
-            </div>
-            <div className="">
-              <Button variant="secondary" className="mr-1 p-2">
-                {' '}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="lucide lucide-trello mr-1 h-5 w-5"
-                >
-                  <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
-                  <rect width="3" height="9" x="7" y="7" />
-                  <rect width="3" height="5" x="14" y="7" />
-                </svg>
-                Boards
-              </Button>
-
-              <Button variant="secondary" className="mr-1 p-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="lucide lucide-heart mr-1 h-5 w-5"
-                >
-                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
-                </svg>
-                Highlights
-              </Button>
-
-              <Button variant="secondary" className="mr-1 p-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="lucide lucide-layout-dashboard mr-1 h-5 w-5"
-                >
-                  <rect width="7" height="9" x="3" y="3" rx="1" />
-                  <rect width="7" height="5" x="14" y="3" rx="1" />
-                  <rect width="7" height="9" x="14" y="12" rx="1" />
-                  <rect width="7" height="5" x="3" y="16" rx="1" />
-                </svg>
-                Views
-              </Button>
-
-              <Button variant="secondary" className="mr-1 p-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="lucide lucide-users-2 mr-1 h-5 w-5"
-                >
-                  <path d="M14 19a6 6 0 0 0-12 0" />
-                  <circle cx="8" cy="9" r="4" />
-                  <path d="M22 19a6 6 0 0 0-6-6 4 4 0 1 0 0-8" />
-                </svg>
-                Members
-              </Button>
-
-              <Button variant="secondary" className="p-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="lucide lucide-settings mr-1 h-5 w-5"
-                >
-                  <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
-                  <circle cx="12" cy="12" r="3" />
-                </svg>
-                Settings
-              </Button>
-            </div>
-          </div>
-          <div className="mb-8 mt-4 grid sm:grid-cols-2 auto-rows-fr gap-2">
-            <Card className=" cursor-pointer bg-slate-200 dark:bg-slate-900">
-              <CardHeader className="flex justify-between gap-4 space-y-0 p-4">
-                <div className="flex justify-between">
-                  <div className="space-y-1">
-                    <CardTitle>Board 1</CardTitle>
-                    <CardDescription>Description</CardDescription>
-                  </div>
-                  <Button variant="ghost">
-                    <StarIcon className="h-6 w-6" />
-                  </Button>
-                </div>
-              </CardHeader>
-            </Card>
-
-            <Card className=" cursor-pointer bg-slate-300 dark:bg-slate-800">
-              <CardHeader className="flex justify-between gap-4 space-y-0 p-4">
-                <div className="flex justify-between">
-                  <div className="space-y-1">
-                    <CardTitle>Board 2</CardTitle>
-                    <CardDescription>
-                      Beautifully designed components built with Radix UI and Tailwind CSS.
-                    </CardDescription>
-                  </div>
-                  <Button variant="ghost">
-                    <StarIcon className="h-6 w-6" />
-                  </Button>
-                </div>
-              </CardHeader>
-            </Card>
-          </div>
-        </div>
+        
       </div>
     </div>
   );
