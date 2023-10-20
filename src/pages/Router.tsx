4f42ffde97269b2b08088a22947ddc22bde19ca6
m.hoseein mirzaei <1380mirza@gmail.com>
2023-10-20 17:32:25 +0330

    chore : changing home URL route

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -16,7 +16,7 @@ const Router = () => {
             <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
         <Route element={<PrivateRoute />}>
-            <Route path="/home" element={<Home/>}/>
+            <Route path="/" element={<Home/>}/>
             <Route path="/board" element={<Board/>}/>
             <Route path="/board/:boardId" element={<Board/>}/>
           </Route>
