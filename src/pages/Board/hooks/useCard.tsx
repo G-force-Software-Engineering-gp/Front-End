4046553289cdad3878d7b6bed3bc133211f7e573
Aryan Sabet Nejad <74782534+aryansabet@users.noreply.github.com>
2023-10-20 13:21:06 +0330

    fix: add AuthTokens instead of mock token

--- src/pages/Board/hooks/useCard.tsx
+++ src/pages/Board/hooks/useCard.tsx
@@ -1,16 +1,17 @@
+import AuthContext from '@/contexts/AuthContext';
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card } from '../types';
-import { new_token } from './useBoard';
 
 export const useCard = (cardId: number) => {
+  let authTokens = useContext(AuthContext)?.authTokens;
   const queryRs = useQuery<Card, Error>({
     queryKey: ['card', cardId],
     queryFn: async () => {
       const response = await fetch(`https://amirmohammadkomijani.pythonanywhere.com/tascrum/card/${cardId}`, {
         method: 'GET',
         headers: {
-          Authorization: `JWT ` + new_token,
+          Authorization: `JWT ` + authTokens,
         },
       });
       const data = await response.json();
