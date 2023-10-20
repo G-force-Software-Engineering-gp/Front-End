fac2fd2f071c213f15ae8a2257969b3b4c60d6e5
m.hoseein mirzaei <1380mirza@gmail.com>
2023-10-20 12:33:22 +0330

    chore : change restriction on login password

--- src/pages/loginSignup/Login.tsx
+++ src/pages/loginSignup/Login.tsx
@@ -14,10 +14,7 @@ import AuthContext , { AuthContextType } from '@/contexts/AuthContext';
 
 const loginSchema = z.object({
   email: z.string().email('Invalid email format').min(1, 'Email is required'),
-  password: z.string()
-    .min(8, 'Password must be at least 8 characters long')
-    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter'),
+  password: z.string().min(1, 'password is required'),
 });
 
 type LoginFormValues = z.input<typeof loginSchema>;
