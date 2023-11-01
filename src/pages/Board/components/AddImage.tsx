3fc886ae7a216d79e79d52ec0524ba8b88d14d8b
matin2001 <matin1380day@gmail.com>
2023-11-01 15:55:56 +0330

    feat: getting the image added

--- src/pages/Board/components/AddImage.tsx
+++ src/pages/Board/components/AddImage.tsx
@@ -9,16 +9,62 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
+import AuthContext from '@/contexts/AuthContext';
 import { DialogClose } from '@radix-ui/react-dialog';
+import axios from 'axios';
 import { ImagePlus } from 'lucide-react';
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const AddImage = () => {
   const { boardId } = useParams();
-  console.log(boardId);
+  // console.log(boardId);
 
-  
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(event)
+    const file = event.target.files?.[0];
+    if (file) {
+      setSelectedImage(file);
+    }
+  };
+  // console.log(selectedImage)
+  let authTokens = useContext(AuthContext)?.authTokens;
+  const handleUpload = async () => {
+    if (selectedImage) {
+      const formData = new FormData();
+      formData.append('image', selectedImage);
+      console.log(formData)
+      const { data } = await axios
+        .put(`https://amirmohammadkomijani.pythonanywhere.com/tascrum/board/${boardId}`, {
+          headers: {
+            Authorization: `JWT ${authTokens.access}`,
+          },
+          body: {
+            backgroundImage: formData,
+          },
+        })
+        .then((response) => response);
+      console.log(data);
+
+      // fetch('your_backend_endpoint', {
+      //   method: 'POST',
+      //   body: formData,
+      // })
+      //   .then((response) => {
+      //     if (response.ok) {
+      //       console.log('Image uploaded successfully.');
+      //     } else {
+      //       console.error('Failed to upload image.');
+      //     }
+      //   })
+      //   .catch((error) => {
+      //     console.error('Error:', error);
+      //   });
+    }
+  };
+
   return (
     <>
       <Dialog>
@@ -39,11 +85,12 @@ const AddImage = () => {
               type="file"
               className=" cursor-pointer placeholder:text-white"
               accept=".jpg, .jpeg, .png"
+              onChange={handleFileChange}
             />
           </div>
           <DialogFooter className="sm:justify-start">
             <DialogClose asChild>
-              <Button type="button" variant="secondary">
+              <Button type="button" variant="secondary" onClick={handleUpload}>
                 Change Picture
               </Button>
             </DialogClose>
