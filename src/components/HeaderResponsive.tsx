6c8f5461159893f4620e80af96932d1623a6eafc
matin2001 <matin1380day@gmail.com>
2023-11-09 12:24:59 +0330

    feat: responsive header and sidebar added to the project

---
+++ src/components/HeaderResponsive.tsx
@@ -0,0 +1,54 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import AuthContext from '@/contexts/AuthContext';
+import { cn } from '@/lib/utils';
+import AddWorkSapceHeader from '@/pages/HomePage/components/AddWorkSapceHeader';
+import axios from 'axios';
+import { AlignLeft, BellDot, HelpCircle, User2 } from 'lucide-react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import HeaderSheet from './HeaderSheet';
+import { Input } from './ui/input';
+import { ModeToggle } from './ui/mode-toggle';
+
+const HeaderResponsive = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <div className="flex-col border-b-2 md:hidden">
+        <div className="flex-1 p-2 px-5 ">
+          <div className="flex items-center justify-between">
+            <div className="text-2xl font-bold">
+              <>
+                <NavigationMenu>
+                  <NavigationMenuList>
+                    <NavigationMenuItem>
+                      <HeaderSheet />
+                    </NavigationMenuItem>
+                  </NavigationMenuList>
+                </NavigationMenu>
+              </>
+            </div>
+            <div className="flex items-center space-x-2">
+              {/* <Input type="text" placeholder="Search" /> */}
+              <ModeToggle data-testid="mode-toggle11" />
+              <BellDot className="h-5 w-5" />
+              <HelpCircle className="h-5 w-5" />
+              <User2 data-testid="user-icon" className="h-5 w-5" onClick={() => navigate('/settings')} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderResponsive;
--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import HeaderResponsive from '@/components/HeaderResponsive';
 import HomePageDetails from './HomePageDetails';
 import { HomeSideBar } from './HomeSideBar';
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
       <Header />
+      <HeaderResponsive />
       <div className="grid grid-flow-col grid-cols-4 justify-center sm:px-52">
         <div className=" hidden sm:col-span-1 sm:block">
           <HomeSideBar />
