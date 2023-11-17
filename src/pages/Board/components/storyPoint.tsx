25484e664536ef357f5496aa7eca1222d4a9c056
m.hoseein mirzaei <1380mirza@gmail.com>
2023-11-17 13:26:24 +0330

    chore: changing variable to int from string

--- src/pages/Board/components/storyPoint.tsx
+++ src/pages/Board/components/storyPoint.tsx
@@ -74,7 +74,7 @@ import { Label } from '@/components/ui/label';
 import { Pen } from 'lucide-react';
 
 interface Props {
-  storyPoint?: string;
+  storyPoint?: number;
   setStoryPoint: any;
 }
 
@@ -102,56 +102,60 @@ export function StoryPointComponent({ storyPoint, setStoryPoint }: Props) {
                   Story Points
                 </Label>
               </div>
-              <Input disabled className="peer z-0  cursor-auto bg-transparent ring-1" placeholder={storyPoint} />
+              <Input
+                disabled
+                className="peer z-0  cursor-auto bg-transparent ring-1"
+                placeholder={storyPoint ? storyPoint.toString() : ''}
+              />
             </div>
             <div className="grid grid-cols-7  gap-3">
               <div className="col-span-1 items-center">
-                <Button onClick={() => setStoryPoint('1')} variant="outline" className="w-auto">
+                <Button onClick={() => setStoryPoint(1)} variant="outline" className="w-auto">
                   1
                 </Button>
               </div>
               <div className="col-span-1 items-center">
-                <Button onClick={() => setStoryPoint('2')} variant="outline">
+                <Button onClick={() => setStoryPoint(2)} variant="outline">
                   2
                 </Button>
               </div>
               <div className="col-span-1 items-center">
-                <Button onClick={() => setStoryPoint('3')} variant="outline">
+                <Button onClick={() => setStoryPoint(3)} variant="outline">
                   3
                 </Button>
               </div>
               <div className="col-span-1 items-center">
-                <Button onClick={() => setStoryPoint('5')} variant="outline">
+                <Button onClick={() => setStoryPoint(5)} variant="outline">
                   5
                 </Button>
               </div>
               <div className="col-span-1 items-center">
-                <Button onClick={() => setStoryPoint('7')} variant="outline">
+                <Button onClick={() => setStoryPoint(7)} variant="outline">
                   7
                 </Button>
               </div>
               <div className="col-span-1 items-center">
-                <Button onClick={() => setStoryPoint('11')} variant="outline">
+                <Button onClick={() => setStoryPoint(11)} variant="outline">
                   11
                 </Button>
               </div>
               <div className="col-span-1 items-center">
-                <Button onClick={() => setStoryPoint('13')} variant="outline">
+                <Button onClick={() => setStoryPoint(13)} variant="outline">
                   13
                 </Button>
               </div>
               <div className="col-span-1 col-start-1 items-center">
-                <Button onClick={() => setStoryPoint('17')} variant="outline">
+                <Button onClick={() => setStoryPoint(17)} variant="outline">
                   17
                 </Button>
               </div>
               <div className="col-span-1 col-end-8 items-center">
-                <Button onClick={() => setStoryPoint('19')} variant="outline">
+                <Button onClick={() => setStoryPoint(19)} variant="outline">
                   19
                 </Button>
               </div>
               <div className="col-span-3 col-end-6 items-center rounded-md border border-destructive text-destructive">
-                <Button onClick={() => setStoryPoint('')} variant="outline" className="w-full">
+                <Button onClick={() => setStoryPoint(undefined)} variant="outline" className="w-full">
                   Remove
                 </Button>
               </div>
