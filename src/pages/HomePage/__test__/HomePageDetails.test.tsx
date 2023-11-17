7ade9e4052075d56d8a00be9aba9bf9741f90b01
matin2001 <matin1380day@gmail.com>
2023-11-17 12:24:26 +0330

    feat: Homepage Sidebar and details tests are completed

--- src/pages/HomePage/__test__/HomePageDetails.test.tsx
+++ src/pages/HomePage/__test__/HomePageDetails.test.tsx
@@ -1,5 +1,5 @@
 import { AuthProvider } from '@/contexts/AuthContext';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import HomePageDetails from '../HomePageDetails';
 
@@ -47,4 +47,128 @@ describe('HomePageDetails Component', () => {
     expect(screen.getByText('Your WorkSpaces')).toBeInTheDocument();
   });
 
+  test('renders Buttons for every WorkSpace', async () => {
+    render(
+      <MemoryRouter>
+        <AuthProvider>
+          <HomePageDetails />
+        </AuthProvider>
+      </MemoryRouter>
+    );
+
+    waitFor(() => {
+      const buttonElement = screen.getByText('Boards');
+      expect(buttonElement).toBeInTheDocument();
+    });
+    waitFor(() => {
+      const buttonElement = screen.getByText('Boards');
+      expect(buttonElement).toHaveClass('mr-1 mt-1 p-2 sm:mt-0');
+    });
+  });
+
+  test('displays a "Create a new Board" button', () => {
+    render(
+      <MemoryRouter>
+        <AuthProvider>
+          <HomePageDetails />
+        </AuthProvider>
+      </MemoryRouter>
+    );
+    waitFor(() => {
+      expect(screen.getByText('Create a new Board')).toBeInTheDocument();
+    });
+  });
+
+  test('displays a message when there are no workspaces', () => {
+    const mockWorkspaces: [] = [];
+    jest.mock('axios', () => ({
+      get: async () => ({ data: mockWorkspaces }),
+    }));
+    render(
+      <MemoryRouter>
+        <AuthProvider>
+          <HomePageDetails />
+        </AuthProvider>
+      </MemoryRouter>
+    );
+    waitFor(() => {
+      expect(screen.getByText('No workspaces found.')).toBeInTheDocument();
+    });
+  });
+
+  test('displays workspace boards', () => {
+    // Mock data for workspaces and boards
+    const mockWorkspaces = [
+      {
+        name: 'Workspace1',
+        boards: [
+          { id: 1, title: 'Board 1', backgroundImage: 'image1.jpg', has_star: false },
+          { id: 2, title: 'Board 2', backgroundImage: 'image2.jpg', has_star: true },
+        ],
+      },
+    ];
+    render(
+      <MemoryRouter>
+        <AuthProvider>
+          <HomePageDetails />
+        </AuthProvider>
+      </MemoryRouter>
+    );
+    // console.log(JSON.parse(localStorage.getItem('authTokens'))?.access);
+    const authTokens = {
+      access:
+        'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5MDkxMTEwLCJpYXQiOjE2OTkwMDQ3MTAsImp0aSI6ImJmYjYxZTc4ZjZmODQyOTFhNGZiN2MwNGI5ZGU4ZjEwIiwidXNlcl9pZCI6MX0.DhQeSSxFcrl8p7mIUnSHL1X4as4UeogzJAW1e8Skabk',
+    };
+    jest.mock('@/contexts/AuthContext', () => ({
+      useContext: () => ({ authTokens }),
+    }));
+
+    // Mocking the Axios call to fetch workspaces
+    jest.mock('axios', () => ({
+      get: async () => ({ data: mockWorkspaces }),
+    }));
+
+    // Check if board titles are displayed
+    waitFor(() => {
+      expect(screen.getByText('Board 1')).toBeInTheDocument();
+    });
+    waitFor(() => {
+      expect(screen.getByText('Board 2')).toBeInTheDocument();
+    });
+  });
+
+  test('navigates to the correct board when a board is clicked', () => {
+    // Mock data for a single workspace and board
+    const mockWorkspaces = [
+      {
+        name: 'Workspace1',
+        boards: [{ id: 1, title: 'Board 1', backgroundImage: 'image1.jpg' }],
+      },
+    ];
+    jest.mock('axios', () => ({
+      get: async () => ({ data: mockWorkspaces }),
+    }));
+
+    // Mock navigate function from react-router-dom
+    const mockNavigate = jest.fn();
+    jest.mock('react-router-dom', () => ({
+      useNavigate: () => mockNavigate,
+    }));
+
+    render(
+      <MemoryRouter>
+        <AuthProvider>
+          <HomePageDetails />
+        </AuthProvider>
+      </MemoryRouter>
+    );
+
+    // Click on the board
+    waitFor(() => {
+      const boardElement = screen.getByText('Board 1');
+      fireEvent.click(boardElement);
+      expect(mockNavigate).toHaveBeenCalledWith('/board/1');
+    });
+    // Ensure it navigates to the correct board path
+  });
 });
