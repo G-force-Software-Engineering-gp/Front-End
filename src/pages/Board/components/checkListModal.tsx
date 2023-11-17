83d1bbd18ac9438a7f34e1e35282570e4a0eb7a1
m.hoseein mirzaei <1380mirza@gmail.com>
2023-11-17 13:28:25 +0330

    fix: changing mock getting data to API call for creating  and updating item

--- src/pages/Board/components/checkListModal.tsx
+++ src/pages/Board/components/checkListModal.tsx
@@ -2,9 +2,12 @@ import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
+import AuthContext from '@/contexts/AuthContext';
 import { Label } from '@radix-ui/react-label';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import _ from 'lodash';
 import { CheckSquare } from 'lucide-react';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import {
   CheckListOption as CheckListOptionType,
@@ -12,104 +15,136 @@ import {
   CheckList as CheckListType,
 } from '../types';
 
-const serverData: CheckListsType = {
-  card: 10,
-  checkLists: [
-    {
-      id: 1,
-      title: 'first checklist',
-      options: [
-        {
-          id: 1,
-          label: 'Item 1',
-          checked: false,
-        },
-        {
-          id: 2,
-          label: 'Item 2',
-          checked: false,
-        },
-        {
-          id: 3,
-          label: 'Item 3',
-          checked: false,
-        },
-        {
-          id: 4,
-          label: 'Item 4',
-          checked: true,
-        },
-        {
-          id: 5,
-          label: 'Item 5',
-          checked: false,
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: 'second checklist',
-      options: [
-        {
-          id: 1,
-          label: 'Item 1',
-          checked: false,
-        },
-        {
-          id: 2,
-          label: 'Item 2',
-          checked: true,
-        },
-        {
-          id: 3,
-          label: 'Item 3',
-          checked: true,
-        },
-        {
-          id: 4,
-          label: 'Item 4',
-          checked: true,
-        },
-        {
-          id: 5,
-          label: 'Item 5',
-          checked: false,
-        },
-      ],
-    },
-  ],
-};
+// const serverData: CheckListsType = {
+//   card: 10,
+//   checkLists: [
+//     {
+//       id: 1,
+//       title: 'first checklist',
+//       options: [
+//         {
+//           id: 1,
+//           label: 'Item 1',
+//           checked: false,
+//         },
+//         {
+//           id: 2,
+//           label: 'Item 2',
+//           checked: false,
+//         },
+//         {
+//           id: 3,
+//           label: 'Item 3',
+//           checked: false,
+//         },
+//         {
+//           id: 4,
+//           label: 'Item 4',
+//           checked: true,
+//         },
+//         {
+//           id: 5,
+//           label: 'Item 5',
+//           checked: false,
+//         },
+//       ],
+//     },
+//     {
+//       id: 2,
+//       title: 'second checklist',
+//       options: [
+//         {
+//           id: 1,
+//           label: 'Item 1',
+//           checked: false,
+//         },
+//         {
+//           id: 2,
+//           label: 'Item 2',
+//           checked: true,
+//         },
+//         {
+//           id: 3,
+//           label: 'Item 3',
+//           checked: true,
+//         },
+//         {
+//           id: 4,
+//           label: 'Item 4',
+//           checked: true,
+//         },
+//         {
+//           id: 5,
+//           label: 'Item 5',
+//           checked: false,
+//         },
+//       ],
+//     },
+//   ],
+// };
 
 interface CheckListOptionsProps {
-  checkListOptions: CheckListOptionType[];
+  checkList: CheckListType;
 }
-const CheckListOptions = ({ checkListOptions }: CheckListOptionsProps) => {
-  const [checklist, setChecklist] = useState(checkListOptions);
+const CheckListOptions = ({ checkList }: CheckListOptionsProps) => {
+  const [checklist, setChecklist] = useState(checkList.items);
+  useEffect(() => {
+    setChecklist(checkList.items);
+  }, [checkList]);
   const [toggleButton, setToggleButton] = useState(false);
+  let authTokens = useContext(AuthContext)?.authTokens;
+  const queryClient = useQueryClient();
+  const createItem = useMutation({
+    mutationFn: (formData: any) => {
+      formData.checklist = checkList?.id;
+      formData.checked = false;
+      console.log(formData);
+      return fetch(`https://amirmohammadkomijani.pythonanywhere.com/tascrum/critem/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `JWT ` + authTokens.access,
+        },
+        body: JSON.stringify(formData),
+      });
+    },
+    onError: (error, variables, context) => {},
+    onSuccess: (data, variables, context) => {},
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['checklist'] });
+    },
+  });
 
   const { register, handleSubmit, reset } = useForm();
 
   const onSubmit = (data: any) => {
-    const newChecklist = [...checklist];
-    newChecklist.push({
-      id: checklist.length + 1,
-      label: data.newCheckbox,
-      checked: false,
-    });
-    setChecklist(newChecklist);
+    // const newChecklist = [...checklist];
+    // newChecklist.push({
+    //   id: checklist.length + 1,
+    //   content: data.newCheckbox,
+    //   checked: false,
+    // });
+    // setChecklist(newChecklist);
+    console.log(data);
+    createItem.mutate(data);
     reset();
   };
   return (
     <div className="m-4 space-y-2 p-4">
       {checklist?.map((item) => (
         <div key={item.id} className="flex flex-row items-center space-x-3">
-          <CheckListItem item={item} checklist={checklist} setChecklist={setChecklist} />
+          <CheckListItem
+            item={item}
+            checklist={checkList.items}
+            checkListId={checkList.id}
+            setChecklist={setChecklist}
+          />
         </div>
       ))}
       {toggleButton && (
         <form onSubmit={handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-          <Textarea
-            {...register('newCheckbox', { required: true })}
+          <Input
+            {...register('content', { required: true })}
             className="block w-full resize-none rounded-md bg-secondary p-2"
             placeholder="Add an item"
           />
@@ -139,12 +174,15 @@ export function CheckListPart({ checklist }: CheckListProps) {
         <CheckSquare className="mb-1 mr-2 h-4 w-4" />
         <Label className="text-md font-semibold">{checklist.title}</Label>
       </div>
-      <CheckListOptions checkListOptions={checklist.options} />
+      <CheckListOptions checkList={checklist} />
     </>
   );
 }
 
-export function CheckListSection() {
+interface CheckListSectionProps {
+  checkLists: CheckListsType;
+}
+export function CheckListSection({ checkLists }: CheckListSectionProps) {
   return (
     <>
       <div className="mt-6 flex items-center justify-between">
@@ -156,7 +194,7 @@ export function CheckListSection() {
           Hide Details
         </Button>
       </div>
-      {serverData.checkLists.map((checkListArrary) => {
+      {checkLists?.checklists.map((checkListArrary) => {
         return <CheckListPart checklist={checkListArrary} />;
       })}
       <div className="ml-10 cursor-pointer"></div>
@@ -165,27 +203,58 @@ export function CheckListSection() {
 }
 type checkListItemProps = {
   item: CheckListOptionType;
+  checkListId: number;
   checklist: any;
   setChecklist: any;
 };
 
-export const CheckListItem = ({ item, checklist, setChecklist }: checkListItemProps) => {
+export const CheckListItem = ({ item, checklist, setChecklist, checkListId }: checkListItemProps) => {
   const [toggleButton, setToggleButton] = useState(true);
+  let authTokens = useContext(AuthContext)?.authTokens;
+  const queryClient = useQueryClient();
+  // useEffect(() => {
+  //   setChecklist(checklist.items);
+  // }, [checklist]);
+  const updateItem = useMutation({
+    mutationFn: (formData: any) => {
+      formData.checklist = checkListId;
+      formData.id = item?.id;
+      console.log(formData);
+      return fetch(`https://amirmohammadkomijani.pythonanywhere.com/tascrum/critem/${item.id}/`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `JWT ` + authTokens.access,
+        },
+        body: JSON.stringify(formData),
+      });
+    },
+    onError: (error, variables, context) => {},
+    onSuccess: (data, variables, context) => {},
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['checklist'] });
+    },
+  });
   const { register, handleSubmit, reset } = useForm();
   const onSubmit = (data: any) => {
     console.log(data);
-    data.id = parseInt(data.id);
-    setChecklist((prevChecklist: any) =>
-      prevChecklist.map((item: CheckListOptionType) => (item.id === data.id ? { ...item, label: data.label } : item))
-    );
+    data.checked = item.checked;
+    // data.id = parseInt(data.id);
+    // setChecklist((prevChecklist: any) =>
+    //   prevChecklist.map((item: CheckListOptionType) => (item.id === data.id ? { ...item, label: data.label } : item))
+    // );
+    updateItem.mutate(data);
     setToggleButton(!toggleButton);
   };
   const handleCheckChange = (itemId: any) => {
-    setChecklist((prevChecklist: any) =>
-      prevChecklist.map((item: CheckListOptionType) =>
-        item.id === itemId ? { ...item, checked: !item.checked } : item
-      )
-    );
+    // setChecklist((prevChecklist: any) =>
+    //   prevChecklist.map((item: CheckListOptionType) =>
+    //     item.id === itemId ? { ...item, checked: !item.checked } : item
+    //   )
+    //);
+    const changedChecked = _.find(checklist, { id: itemId });
+    changedChecked.checked = !changedChecked.checked;
+    updateItem.mutate(changedChecked);
   };
   return (
     <>
@@ -193,15 +262,15 @@ export const CheckListItem = ({ item, checklist, setChecklist }: checkListItemPr
         <>
           <Checkbox checked={item.checked} onCheckedChange={() => handleCheckChange(item.id)} />
           <Label className="cursor-pointer text-sm font-light" onClick={() => setToggleButton(!toggleButton)}>
-            {item.label}
+            {item.content}
           </Label>
         </>
       ) : (
         <form onSubmit={handleSubmit(onSubmit)} className="w-2/3 space-y-6">
           <Input
-            {...register('label', { required: true })}
+            {...register('content', { required: true })}
             className="block w-full resize-none rounded-md bg-secondary p-2"
-            defaultValue={item.label}
+            defaultValue={item.content}
           />
           <Input
             readOnly={true}
