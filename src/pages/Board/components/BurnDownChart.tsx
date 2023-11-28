ad3b3988cc8e7a3dd6c9a705a35f294925b0109a
matin2001 <matin1380day@gmail.com>
2023-11-28 22:17:32 +0330

    feat: dynamic columns and data are handled

--- src/pages/Board/components/BurnDownChart.tsx
+++ src/pages/Board/components/BurnDownChart.tsx
@@ -9,138 +9,108 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import {
-  Column,
   ColumnDef,
   flexRender,
   getCoreRowModel,
-  getFilteredRowModel,
   getPaginationRowModel,
   RowData,
-  Table,
   useReactTable,
 } from '@tanstack/react-table';
-import React from 'react';
+import React, { useState } from 'react';
+import { object } from 'zod';
 
 declare module '@tanstack/react-table' {
   interface TableMeta<TData extends RowData> {
     updateData: (rowIndex: number, columnId: string, value: unknown) => void;
   }
 }
-
-const types = ['Estimate1', 'Done1', 'Estimate2', 'Done2', 'Estimate3', 'Done3'];
-type Person = {
-  // Should be converted to a map
-  Estimate1: number;
-  Done1: number;
-  Estimate2: number;
-  Done2: number;
-  Estimate3: number;
-  Done3: number;
-};
-// type Person = {
-//   [K in typeof types[number]]: number;
-// };
-
-const defaultData: Person[] = [
-  // should be a map
-  {
-    Estimate1: 10,
-    Done1: 12,
-    Estimate2: 5,
-    Done2: 6,
-    Estimate3: 5,
-    Done3: 6,
-  },
-  {
-    Estimate1: 8,
-    Done1: 6,
-    Estimate2: 5,
-    Done2: 6,
-    Estimate3: 5,
-    Done3: 6,
-  },
-  {
-    Estimate1: 4,
-    Done1: 7,
-    Estimate2: 5,
-    Done2: 6,
-    Estimate3: 5,
-    Done3: 6,
-  },
-];
-
-const defaultColumn: Partial<ColumnDef<Person>> = {
-  cell: function Cell({ getValue, row: { index }, column: { id }, table }) {
-    const initialValue = getValue();
-    // We need to keep and update the state of the cell normally
-    const [value, setValue] = React.useState(initialValue);
-    const onBlur = () => {
-      table.options.meta?.updateData(index, id, value);
-    };
-    React.useEffect(() => {
-      setValue(initialValue);
-    }, [initialValue]);
-
-    return <Input value={value as string} onChange={(e) => setValue(e.target.value)} onBlur={onBlur} />;
-  },
-};
+interface LooseObject {
+  [key: string]: any;
+}
 
 function BurnDownChart() {
-  // Should convert columns to a map
-  const columns = React.useMemo<ColumnDef<Person>[]>(
-    () => [
-      {
-        header: 'Matin',
+  type Person = Record<string, number>;
+
+  const [users, setusers] = useState(['Matin', 'Aryan', 'Mirza', 'Javad']);
+
+  const [defaultData, setDefaultData] = useState<any[]>(() => {
+    let initialData: any[] = [];
+    for (let i = 0; i < 3; i++) {
+      var obj: LooseObject = {};
+      obj['date'] = '2023/5/6';
+      for (let j = 0; j < users.length; j++) {
+        obj[`estimate${users[j]}`] = 0;
+        obj[`done${users[j]}`] = 0;
+      }
+      initialData.push(obj);
+    }
+    return initialData;
+  });
+  const columns = React.useMemo<ColumnDef<Person>[]>(() => {
+    let cols = [];
+    cols.push({
+      header: `Date`,
+      accessorKey: `date`,
+      id: `date`,
+    });
+    for (let j = 0; j < users.length; j++) {
+      cols.push({
+        header: `${users[j]}`,
         // footer: (props) => props.column.id,
         columns: [
           {
-            accessorKey: 'Estimate1',
+            accessorKey: `estimate${users[j]}`,
             header: 'Estimate',
-            id: 'Estimate1',
-            footer: (props) => props.column.id,
+            id: `e${users[j]}`,
+            footer: 'Total Estimate',
           },
           {
-            accessorFn: (row) => row.Done1,
-            id: 'Done1',
-            footer: (props) => props.column.id,
+            accessorKey: `done${users[j]}`,
+            header: 'Done',
+            id: `d${users[j]}`,
+            footer: 'Total Done',
           },
         ],
-      },
-      {
-        header: 'Mohmamad',
-        // footer: (props) => props.column.id,
-        columns: [
-          {
-            accessorKey: 'Estimate2',
-            id: 'Estimate2',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorFn: (row) => row.Done2,
-            id: 'Done2',
-            footer: (props) => props.column.id,
-          },
-        ],
-      },
-      {
-        header: 'Aryan',
-        // footer: (props) => props.column.id,
-        columns: [
-          {
-            accessorFn: (row) => row.Estimate3,
-            id: 'Estimate3',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorFn: (row) => row.Done3,
-            id: 'Done3',
-            footer: (props) => props.column.id,
-          },
-        ],
-      },
-    ],
-    []
-  );
+      });
+    }
+    return cols;
+  }, []);
+
+  const defaultColumn: Partial<ColumnDef<Person>> = {
+    cell: function Cell({ getValue, row: { index }, column: { id }, table }) {
+      const initialValue = getValue();
+      // We need to keep and update the state of the cell normally
+      const [value, setValue] = React.useState(initialValue);
+      const onBlur = () => {
+        table.options.meta?.updateData(index, id, value);
+      };
+      React.useEffect(() => {
+        setValue(initialValue);
+      }, [initialValue]);
+
+      // Extracting the username from the column ID
+
+      // Handle input change
+      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // Log only if the input value changes
+        if (e.target.value !== value) {
+          const username = id.substring(1);
+          const state = id[0] === 'd' ? 'done' : 'estimate';
+          console.log('Row: ', defaultData[index]?.date);
+          console.log('New value: ', e.target.value);
+          console.log('User: ', username);
+          console.log('State: ', state);
+        }
+        setValue(e.target.value);
+      };
+
+      return id === 'date' ? (
+        <div>{value as string}</div>
+      ) : (
+        <Input value={value as string} onChange={handleChange} onBlur={onBlur} />
+      );
+    },
+  };
 
   const [data, setData] = React.useState([...defaultData]);
   const table = useReactTable({
@@ -148,7 +118,7 @@ function BurnDownChart() {
     columns,
     defaultColumn,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
+    // getPaginationRowModel: getPaginationRowModel(),
     // Provide our updateData function to our table meta
     meta: {
       updateData: (rowIndex, columnId, value) => {
@@ -167,8 +137,6 @@ function BurnDownChart() {
       },
     },
   });
-  console.log(table.getFooterGroups()[0].headers);
-
   return (
     <div className="p-2">
       <div className="h-2" />
