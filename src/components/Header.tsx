e5a2cef883b7982d9815045752a66653e2cb2cf0
matin2001 <matin1380day@gmail.com>
2024-01-26 17:51:45 +0330

    fit: question mark details added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,9 +13,12 @@ import { BaseURL } from '@/pages/baseURL';
 import AddWorkSapceHeader from '@/pages/HomePage/components/AddWorkSapceHeader';
 import { useRecentlyViewed } from '@/pages/HomePage/hooks/useRecentlyViewed';
 import axios from 'axios';
-import { BellDot, ClipboardList, HelpCircle, User2 } from 'lucide-react';
+import { BellDot, CalendarDays, ClipboardList, HelpCircle, Layers, User2 } from 'lucide-react';
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { Button } from './ui/button';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card';
 import { Input } from './ui/input';
 import { ModeToggle } from './ui/mode-toggle';
 
@@ -251,9 +254,98 @@ const Header = () => {
               <div className="flex items-center space-x-2">
                 <Input type="text" placeholder="Search" />
                 <ModeToggle data-testid="mode-toggle11" />
-                <BellDot className="h-9 w-9" />
-                <HelpCircle className="h-9 w-9" />
-                <User2 data-testid="user-icon" className="h-9 w-9" onClick={() => navigate('/settings')} />
+                {/* <BellDot className="h-9 w-9" /> */}
+
+                <HoverCard>
+                  <HoverCardTrigger asChild>
+                    <HelpCircle className="h-9 w-12 cursor-pointer rounded-md px-[6px] transition-colors hover:bg-accent" />
+                  </HoverCardTrigger>
+                  <HoverCardContent className="mr-10 grid w-[40vw] grid-cols-2">
+                    <div className="mb-4 flex justify-between space-x-4">
+                      <Avatar>
+                        <AvatarImage src="https://github.com/vercel.png1" />
+                        <AvatarFallback><Layers /></AvatarFallback>
+                      </Avatar>
+                      <div className="space-y-1">
+                        <h4 className="text-sm font-semibold">Workspace Management</h4>
+                        <p className="text-sm">
+                          Organize your projects into distinct workspaces, making it easy to categorize and manage
+                          multiple projects at once.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="mb-4 flex justify-between space-x-4">
+                      <Avatar>
+                        <AvatarImage src="https://github.com/vercel.png1" />
+                        <AvatarFallback><ClipboardList /></AvatarFallback>
+                      </Avatar>
+                      <div className="space-y-1">
+                        <h4 className="text-sm font-semibold">Interactive Boards</h4>
+                        <p className="text-sm">
+                          Dive deep into each workspace with dedicated boards. Customize them to fit your workflow and
+                          visualize your tasks like never before.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="mb-4 flex justify-between space-x-4">
+                      <Avatar>
+                        <AvatarImage src="https://github.com/vercel.png1" />
+                        <AvatarFallback>VC</AvatarFallback>
+                      </Avatar>
+                      <div className="space-y-1">
+                        <h4 className="text-sm font-semibold">Task Assignment</h4>
+                        <p className="text-sm">
+                          Efficiently delegate tasks to team members and keep everyone on the same page with real-time
+                          updates and notifications.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="mb-4 flex justify-between space-x-4">
+                      <Avatar>
+                        <AvatarImage src="https://github.com/vercel.png1" />
+                        <AvatarFallback>VC</AvatarFallback>
+                      </Avatar>
+                      <div className="space-y-1">
+                        <h4 className="text-sm font-semibold">Calendar Integration</h4>
+                        <p className="text-sm">
+                          Stay on schedule with our integrated calendar feature. Plan your tasks, set reminders, and
+                          track deadlines with ease.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex justify-between space-x-4">
+                      <Avatar>
+                        <AvatarImage src="https://github.com/vercel.png1" />
+                        <AvatarFallback>VC</AvatarFallback>
+                      </Avatar>
+                      <div className="space-y-1">
+                        <h4 className="text-sm font-semibold">Burndown Chart</h4>
+                        <p className="text-sm">
+                          Monitor your project's progress with our intuitive burndown charts. Gain insights into task
+                          completion rates and adjust your strategies accordingly.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex justify-between space-x-4">
+                      <Avatar>
+                        <AvatarImage src="https://github.com/vercel.png1" />
+                        <AvatarFallback>VC</AvatarFallback>
+                      </Avatar>
+                      <div className="space-y-1">
+                        <h4 className="text-sm font-semibold">Timeline View</h4>
+                        <p className="text-sm">
+                          Visualize your project's timeline and milestones. Understand project progression and identify
+                          potential bottlenecks before they arise.
+                        </p>
+                      </div>
+                    </div>
+                  </HoverCardContent>
+                </HoverCard>
+                <User2
+                  data-testid="user-icon"
+                  className="h-9 w-12 cursor-pointer rounded-md px-[6px] transition-colors hover:bg-accent"
+                  onClick={() => navigate('/settings')}
+                />
               </div>
             </div>
           </div>
